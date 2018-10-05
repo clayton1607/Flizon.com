54aa771e5066d8606f1ca4a33ec2240280e98522
Clayton <claytonpereira1998@gmail.com>
2018-10-05 20:30:50 +0530

    hello

--- Server/views.js
+++ Server/views.js
@@ -10,10 +10,26 @@ const bodyParser = require('body-parser');
 var app =express();
 //ar {authenticate}=require('./middleware/authenticate.js');
 var {users}=require('./Tables/users.js');
-app.use(express.static(__dirname+'/Public/'));
+app.use(express.static(__dirname+'/Public'));
 app.use(bodyParser.json());
 console.log(__dirname+'/Public');
 app.set('view engine','hbs');
 
 //app.get('',(req,res)=>{});
+app.get('/',(req,res)=>{
+  res.render('test/home.hbs');
+});
+app.get('/home',(req,res)=>{
+  res.render('test/home.hbs');
+});
+
+app.get('/faq',(req,res)=>{
+  res.render('test/faq.hbs');
+});
+app.get('/about',(req,res)=>{
+  res.render('test/about.hbs');
+});
+app.get('/contact',(req,res)=>{
+  res.render('test/contact.hbs');
+});
 module.exports={app};
